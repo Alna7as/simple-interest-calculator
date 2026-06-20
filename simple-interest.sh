fef5943d8291439909698f8561e8bfb088538f39
يوسف ابو الحسن عبدالمنعم عبدالعال <104838310+Alna7as@users.noreply.github.com>
2026-06-20 14:19:54 +0300

    Add simple interest calculator script
    This script calculates simple interest based on user input for principal, rate, and time.

---
+++ simple-interest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Simple Interest Calculator
+
+echo "Enter principal:"
+read p
+
+echo "Enter rate of interest:"
+read r
+
+echo "Enter time:"
+read t
+
+s=$((p*r*t/100))
+
+echo "Simple Interest is $s"
